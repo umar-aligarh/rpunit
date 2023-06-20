8f9e98480c179b59dc97efc5d2a73e6b38d25e17
Umar <uk.umarkhan1999@gamil.com>
2023-06-20 12:30:15 +0530

    modifications

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ mongoose.connect('mongodb+srv://ukumarkhan1999:dAYC3lwx8oS0j69q@cluster0.b2ghbvr
     (err)=>{
         console.log('database connection error:',err)
     }
-
 );
+mongoose.pluralize(null);
 
 app.set('view engine', 'ejs');
 app.set('views',__dirname + '/views');
 
-app.use('/static', express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 app.use(bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}));
 
-let port = "3000"
+let port = "7000"
 async function saveResultInTest(en ,fac)
 {
     const url = "https://ctengg.amu.ac.in/web/table_result010.php";
@@ -72,16 +72,34 @@ async function savetoDatabase(htmlString)
             name: studentName,
             marks:[sessionalMarks,examMarks,graceMarks]
         }};
-        mongoose.pluralize(null);
         studentModel = mongoose.model(courseName,marksSchema);
         await studentModel.findOneAndUpdate(filter, update,{upsert: true});
     }
-
 }
+app.get("/coursemarks",async(req,res)=>{
+    res.write('<!DOCTYPE html><html><head><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script><link rel="stylesheet" href="table.css"></head><body>');
+    res.write('<div class="container"><div class="table"><div class="table-header">');
+    res.write('<div class="header__item"><a id="name" class="filter__link" href="#">Name</a></div>');
+    res.write('<div class="header__item"><a id="sessional" class="filter__link" href="#">Sessional Marks</a></div>');
+    res.write('<div class="header__item"><a id="end" class="filter__link" href="#">Exam Marks</a></div>');
+    res.write('<div class="header__item"><a id="total" class="filter__link" href="#">Total Marks</a></div></div><div class="table-content">');
 
+    courseName = req.query.course;
+    courseName = courseName.toUpperCase();
+    console.log(courseName);
+    studentModel = mongoose.model(courseName,marksSchema);
+    const filter = {};
+    const all = await studentModel.find(filter);
+    console.log(all);
+    for(let i=0;i<all.length;i++)
+    {
+        res.write(`<div class="table-row"><div class="table-data">${all[i].name}</div><div class="table-data">${all[i].marks[0]}</div><div class="table-data">${parseInt(all[i].marks[1])+parseInt(all[i].marks[2])}</div><div class="table-data">${parseInt(all[i].marks[0])+parseInt(all[i].marks[1])+parseInt(all[i].marks[2])}</div></div>`);
+    }
+    res.write('</div></div></div><script language="JavaScript" type="text/javascript" src="tablescript.js"></script></body></html>');
+    res.end();
+})
 
-
-app.get("/",async(req,res)=>{
+app.get("/xyz",async(req,res)=>{
     let csvString = await fs.readFile(__dirname+"/AMS2630.csv",'utf8');
     const enRegexp = /[ghijklmn]{2}[0-9]{4}/ig;
     const facRegexp = /[0-9]{2}[a-z]{3}[0-9]{3}/ig
@@ -95,13 +113,14 @@ app.get("/",async(req,res)=>{
         let htmlString = await fs.readFile(__dirname+"/public/test",'utf8');
         await savetoDatabase(htmlString);
     }
-
-
     res.send('done');
+})
+app.get("/",async(req,res)=>{
+    res.sendFile(__dirname+'/public/home.html');
 })
 
 
-app.listen(port, () => console.log("Server started on port 3000"));
+app.listen(port, () => console.log("Server started on port 7000"));
 
 
 
